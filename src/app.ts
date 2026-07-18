d5d80a93eb058ce35bc23263dc74e8beea266894
sohamnesarikar <nesarikarsoham24@gmail.com>
2026-07-18 20:07:37 +0530

    basic types done

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+// number type
+let age: number = 23;
+age = 31;
+// age = "43"
+console.log(age);
+
+// string type
+let username: string = "john";
+username = "jane";
+// username = 88
+console.log(username);
+
+// boolean type
+let isActive: boolean = true;
+isActive = false;
+// isActive = "false"
+console.log(isActive);
